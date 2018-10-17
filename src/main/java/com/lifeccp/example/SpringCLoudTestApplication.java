f71c6a01baafa01d8fc623bbe76b014ebdd07f8e
Dou Hao <douhao@wingspan.cn>
2018-10-17 18:40:07 +0800

    init

---
+++ src/main/java/com/lifeccp/example/SpringCLoudTestApplication.java
@@ -0,0 +1,12 @@
+package com.lifeccp.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCLoudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCLoudTestApplication.class, args);
+	}
+}
